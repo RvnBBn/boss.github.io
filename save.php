a208e458fa6f140635eb7ec76cd15d9a1139e856
RvnBBn <rvnbbn04@gmail.com>
2023-07-17 00:23:19 +0300

    added files

--- save.php
+++ save.php
@@ -2,18 +2,11 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 
-// Create a string with the form data
+// Create a string with the user input
 $data = "Name: $name\nEmail: $email\nMessage: $message\n\n";
 
 // Define the path to the text file
-$filePath = 'form_data.txt';
+$file_path = "user_input.txt";
 
-// Open the file in append mode and write the data
-$file = fopen($filePath, 'a');
-if ($file) {
-    fwrite($file, $data);
-    fclose($file);
-    echo 'Form submitted successfully!';
-} else {
-    echo 'Unable to open file.';
-}
+// Write the data to the file
+file_put_contents($file_path, $data, FILE_APPEND | LOCK_EX);
--- script.js
+++ script.js
@@ -19,7 +19,35 @@ function changeText() {
     element.innerHTML = "<a href>Text has been changed!</a>";
     isTextChanged = true;
   }
+
 }
+  // Function to handle form submission
+  function submitForm(event) {
+    event.preventDefault(); // Prevent default form submission behavior
+
+    // Retrieve form input values
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    // Create an HTTP request object
+    var xhr = new XMLHttpRequest();
+
+    // Set up the request
+    xhr.open('POST', 'store_input.php', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+    // Define the data to be sent
+    var data = 'name=' + encodeURIComponent(name) +
+               '&email=' + encodeURIComponent(email) +
+               '&message=' + encodeURIComponent(message);
+
+    // Send the request
+    xhr.send(data);
+  }
+
+  // Attach the submitForm function to the form's submit event
+ document.getElementById('userForm').addEventListener('submit', submitForm);
   
 
 
